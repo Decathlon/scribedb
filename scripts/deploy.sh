412f3e5dae762537b6f74263d47acbd2762f48a9
Fawaz PARAISO <fawaz.paraiso@decathlon.com>
2020-03-16 18:50:42 +0100

    Change CI/DC life cylcle

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,5 +5,5 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 echo 'Docker Login - END'
 
 echo 'Docker Push - BEGIN'
-docker push ossdecathlon/scribedb:$COMMIT
+docker push ossdecathlon/scribedb:$TAG
 echo 'Docker Push - END'
