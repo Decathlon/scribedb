dd579c04fc9f2e61e4623da321ec1e6f78e8da71
Fawaz PARAISO <fawaz.paraiso@decathlon.com>
2020-03-17 14:54:35 +0100

    CI Remove unused file

--- scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo 'Docker Login - BEGIN'
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-echo 'Docker Login - END'
-
-echo 'Docker Push - BEGIN'
-docker push ossdecathlon/scribedb:$TAG
-echo 'Docker Push - END'
