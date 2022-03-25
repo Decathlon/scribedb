b8e098c8517b5512fd77855d59dbd5877b5820f3
Pierre-Marie Petit <pierremarie.petit.partner@decathlon.com>
2022-03-25 18:11:11 +0100

    add udaf

--- scribedb/__main__.py
+++
@@ -1,5 +0,0 @@
-"""Allow moulinette to be executable through `python -m moulinette`."""
-from .scribedb import scribedb  # pragma: no cover
-
-if __name__ == "__main__":  # pragma: no cover
-    scribedb()
